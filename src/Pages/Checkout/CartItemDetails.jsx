82954f12704285c69105f9d9e32dbc57f6eaf821
Sarmad Faheem <muhammadsarmad302@gmail.com>
2025-10-21 15:30:52 +0500

    Add functionality to update in OrderSummary

--- src/Pages/Checkout/CartItemDetails.jsx
+++ src/Pages/Checkout/CartItemDetails.jsx
@@ -4,19 +4,29 @@ import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
   const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
 
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   };
 
-  const updateQuantity = () => {
-    // Switch between true and false for isUpdatingQuantity.
+  const updateQuantity = async () => {
     if (isUpdatingQuantity) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity),
+      });
+
+      await loadCart();
+
       setIsUpdatingQuantity(false);
     } else {
       setIsUpdatingQuantity(true);
     }
+  };
+
+  const getQuantity = (event) => {
+    setQuantity(event.target.value);
   };
 
   return (
@@ -32,7 +42,12 @@ export function CartItemDetails({ cartItem, loadCart }) {
           <span>
             Quantity:
             {isUpdatingQuantity ? (
-              <input type="text" className="quantity-textbox" />
+              <input
+                type="text"
+                className="quantity-textbox"
+                value={quantity}
+                onChange={getQuantity}
+              />
             ) : (
               <span className="quantity-label">{cartItem.quantity}</span>
             )}
