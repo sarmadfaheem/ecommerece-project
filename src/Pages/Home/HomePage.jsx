5ecc086528b095a1788f91ef916234186458fa9e
Sarmad Faheem <muhammadsarmad302@gmail.com>
2025-10-21 11:17:54 +0500

    Create Add to cart Feature

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -4,7 +4,7 @@ import { Header } from "../../Components/Header";
 import { ProductGrid } from "./ProductGrid";
 import "./HomePage.css";
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -14,7 +14,6 @@ export function HomePage({ cart }) {
     };
 
     getHomeData();
-
   }, []);
 
   return (
@@ -26,7 +25,7 @@ export function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-        <ProductGrid products={products} />
+        <ProductGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
--- src/Pages/Order/OrdersPage.jsx
+++ src/Pages/Order/OrdersPage.jsx
@@ -80,7 +80,7 @@ export function OrdersPage({ cart }) {
                         </div>
 
                         <div className="product-actions">
-                          <a href="/tracking">
+                          <a href="">
                             <button className="track-package-button button-secondary">
                               Track package
                             </button>
